b5b0eed6aeb0c03ac0bb76605383e05fe4a9a655
Søren <iammenqu@gmail.com>
2022-10-27 11:09:08 +0200

    improve styling

--- src/components/ChatWindow/MessageDisplay/index.tsx
+++ src/components/ChatWindow/MessageDisplay/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useContext, useStyles$ } from '@builder.io/qwik'
+import { component$, useContext, useStyles$, useRef, useClientEffect$, useSignal, Signal, useWatch$ } from '@builder.io/qwik'
 import { ChatContext } from '~/components/ChatWindow'
 import ChatMessage from '../ChatMessage'
 import Styling from './message-display.css'
@@ -6,9 +6,19 @@ import Styling from './message-display.css'
 export default component$(() => {
   useStyles$(Styling)
   const chatContext = useContext(ChatContext)
+  const signal = useSignal<HTMLElement>()
+
+  useClientEffect$(({ track }) => {
+    track(() => chatContext.messages.length);
+
+    if (signal.value){
+      console.log('signal', signal.value)
+      signal.value.scrollTop = signal.value.scrollHeight
+    }
+  })
 
   return (
-    <div class="message-display">
+    <div ref={signal} class="message-display">
       {chatContext.messages.map((message) => (
         <ChatMessage message={message} />
       ))}
