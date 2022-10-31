68b5f02ce6295583ad6c5e29bb56776fc782b0a8
Niklas Kludt <niklas.kludt@icloud.com>
2022-10-31 16:17:51 +0100

    Created webhook for availability

--- backend/fulfillment/webhook.py
+++ backend/fulfillment/webhook.py
@@ -1,21 +1,29 @@
 from flask import Blueprint, request
-from fulfillment.webhookLogic import checkGame
+from fulfillment.webhookLogic import check_game_console, check_game_availability
 
 webhook = Blueprint('webhook', __name__)
 @webhook.route('/webhook', methods=['GET', 'POST'])
 def fulfillment():
     req = request.get_json(silent=True, force=True)
     query_result = req.get('queryResult')
-    fulfillmentText = ''
+    fulfillment_text = ''
 
     if query_result.get('action') == 'order.game':
         game = query_result.get('parameters').get('game')
         console = query_result.get('parameters').get('console')
-        if checkGame(game, console):
-            fulfillmentText = f"You wish to rent {game} for {console}, is that right?"
+        if check_game_console(game, console):
+            fulfillment_text = f"You wish to rent {game} for {console}, is that right?"
         else:
-            fulfillmentText = f"The game {game} is not available on {console}. Please try again."
+            fulfillment_text = f"The game {game} is not available on {console}. Please try again."
+
+    if query_result.get('action') == 'order.game-yes':
+        game = query_result.get('outputContexts')[0].get('parameters').get('game')
+        if check_game_availability(game):
+            fulfillment_text = f"We still have {game} in stock! Pick it up in our opening hours."
+        else:
+            fulfillment_text = f"The game {game} is currently not in stock. Please try again at a later time. "
+
     return {
-        'fulfillmentText': fulfillmentText,
+        'fulfillmentText': fulfillment_text,
         'source': "webhookdata"
     }
--- backend/fulfillment/webhookLogic.py
+++ backend/fulfillment/webhookLogic.py
@@ -2,16 +2,28 @@ import os
 import json
 
 
-def checkGame(gameName, consoleName):
+def check_game_console(gameName, consoleName):
     script_dir = os.path.dirname(__file__)
     json_path = os.path.join(script_dir, '../data/games.json')
     with open(json_path) as data:
         games = json.load(data)
-        print(games)
         for game in games:
             if gameName == game['name']:
                 for console in game['consoles']:
                     if console == consoleName:
                         return True
+
+    return False
+
+
+def check_game_availability(gameName):
+    script_dir = os.path.dirname(__file__)
+    json_path = os.path.join(script_dir, '../data/games.json')
+    with open(json_path) as data:
+        games = json.load(data)
+        for game in games:
+            if gameName == game['name']:
+                if game['available']:
+                    return True
 
     return False
