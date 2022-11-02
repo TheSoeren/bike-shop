4e75353bddd8a5e2445353d8fb27858aa8efe663
Niklas Kludt <niklas.kludt@icloud.com>
2022-11-02 16:17:16 +0100

    Added cors to request

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request
 from dialogflow.access import send_intent
 from fulfillment.webhook import webhook
+from flask_cors import CORS
 
 app = Flask('blockbuster')
 app.register_blueprint(webhook)
-
+CORS(app)
 
 @app.route('/communicate')
 def communicate():
-    session_id = request.args.get('session-id')
-    text = request.args.get('text')
+    session_id = request.args.get('sessionId')
+    text = request.args.get('message')
     language = request.args.get('language')
-    return send_intent(session_id, text, language)
+    resp = app.make_response(send_intent(session_id, text, language))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 if __name__ == '__main__':
