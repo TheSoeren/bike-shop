8ed24c57cc0f790eacddab48b6f88fe676265036
Niklas Kludt <niklas.kludt@icloud.com>
2022-11-03 11:22:10 +0100

    Added multi language functionality to backend

--- backend/fulfillment/webhook.py
+++ backend/fulfillment/webhook.py
@@ -1,47 +1,92 @@
 from flask import Blueprint, request
 from fulfillment.webhookLogic import check_game_console, check_game_availability, check_customer_nr
+import os
+from string import Template
+import json
 
 webhook = Blueprint('webhook', __name__)
 
 
 @webhook.route('/webhook', methods=['GET', 'POST'])
 def fulfillment():
-    # extract request parameters
     req = request.get_json(silent=True, force=True)
     query_result = req.get('queryResult')
     fulfillment_text = ''
 
-    # complete fulfillment for order.game action
     if query_result.get('action') == 'order.game':
-        game = query_result.get('parameters').get('game')
-        console = query_result.get('parameters').get('console')
-        if check_game_console(game, console):
-            fulfillment_text = f"You wish to rent {game} for {console}, is that right?"
-        else:
-            fulfillment_text = f"The game {game} is not available on {console}. Please try again."
-
-    # complete fulfillment for order.game-yes action
-    if query_result.get('action') == 'order.game-yes':
-        game = query_result.get('outputContexts')[0].get('parameters').get('game')
-        if check_game_availability(game):
-            fulfillment_text = f"We still have {game} in stock! Pick it up in our opening hours."
-        else:
-            fulfillment_text = f"The game {game} is currently not in stock. Please try again at a later time. "
-
-    #complete fulfillment for request.rented_games action
-    if query_result.get('action') == 'request.rented_games':
-        customer_nr = query_result.get('parameters').get('customer-nr')
-        customer_games = check_customer_nr(customer_nr)
-        if customer_games == -1:
-            fulfillment_text = f"Your customer number could not be recognized, please try again"
-        elif not customer_games:
-            fulfillment_text = f"You currently have no games rented."
-        else:
-            fulfillment_text = f"You have the following games rented: \n"
-            for game in customer_games:
-                fulfillment_text += f"{game}, \n"
+        fulfillment_text = fulfill_game_console(query_result)
+    elif query_result.get('action') == 'order.game-yes':
+        fulfillment_text = fulfill_game_availability(query_result)
+    elif query_result.get('action') == 'request.rented_games':
+        fulfillment_text = fulfill_customer_nr(query_result)
 
     return {
         'fulfillmentText': fulfillment_text,
         'source': "webhookdata"
     }
+
+
+def load_json():
+    script_dir = os.path.dirname(__file__)
+    json_path = os.path.join(script_dir, 'texts.json')
+    with open(json_path, 'r') as json_file:
+        return json.load(json_file)
+
+
+def fulfill_game_console(query_result):
+    language_code = query_result['languageCode']
+    game = query_result.get('parameters').get('game')
+    console = query_result.get('parameters').get('console')
+    texts = load_json()
+    if check_game_console(game, console):
+        if language_code == 'en':
+            return texts['check_console']['en']['success'].replace('$game$', game).replace('$console$', console)
+        else:
+            return texts['check_console']['de']['success'].replace('$game$', game).replace('$console$', console)
+    else:
+        if language_code == 'en':
+            return texts['check_console']['en']['failure'].replace('$game$', game).replace('$console$', console)
+        else:
+            return texts['check_console']['de']['failure'].replace('$game$', game).replace('$console$', console)
+
+
+def fulfill_game_availability(query_result):
+    language_code = query_result['languageCode']
+    game = query_result.get('outputContexts')[0].get('parameters').get('game')
+    texts = load_json()
+    if check_game_availability(game):
+        if language_code == 'en':
+            return texts['check_availability']['en']['success'].replace('$game$', game)
+        else:
+            return texts['check_availability']['de']['success'].replace('$game$', game)
+    else:
+        if language_code == 'en':
+            return texts['check_availability']['en']['failure'].replace('$game$', game)
+        else:
+            return texts['check_availability']['de']['failure'].replace('$game$', game)
+
+
+def fulfill_customer_nr(query_result):
+    language_code = query_result['languageCode']
+    texts = load_json()
+    customer_nr = query_result.get('parameters').get('customer-nr')
+    customer_games = check_customer_nr(customer_nr)
+    if customer_games == -1:
+        if language_code == 'en':
+            return texts['check_customer_nr']['en']['failure']
+        else:
+            return texts['check_customer_nr']['de']['failure']
+    elif not customer_games:
+        if language_code == 'en':
+            return texts['check_customer_nr']['en']['empty']
+        else:
+            return texts['check_customer_nr']['de']['empty']
+    else:
+        text = ''
+        if language_code == 'en':
+            text = texts['check_customer_nr']['en']['success']
+        else:
+            text = texts['check_customer_nr']['de']['success']
+        for game in customer_games:
+            text += f"{game}, "
+        return text
